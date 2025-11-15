fa8a7625b2819e454218cae6351bb63b5dba833f
Gerry <gerrywiegand@gmail.com>
2025-11-15 14:51:52 -0500

    updated data and data/id routes

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
-from lib.data import data, URL
-import flask from Flask
+from flask import Flask
 
+from lib.data import data
 
 app = Flask(__name__)
 
-class placeholder:
+
+class Inventory:
     def __init__(self):
         pass
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Welcome to the Open Food Facts App!"
 
-@app.route('/data')
-def get_data():
-    return data
 
-if __name__ == '__main__':
+@app.route("/data", methods=["GET"])
+def get_data():
+    return data, 200
+
+
+@app.route("/data/<int:item_id>", methods=["GET"])
+def get_item(item_id):
+    for item in data:
+        if item.get("id") == item_id:
+            return item, 200
+    return {"error": "Item not found"}, 404
+
+
+if __name__ == "__main__":
     app.run(debug=True)
